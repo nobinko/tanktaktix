78cd515d18fe7450aa9e191d33f8e30721af90f2
nobinko <195414465+nobinko@users.noreply.github.com>
2026-01-29 22:38:10 +0900

    Fix dev websocket proxy via Vite

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,6 +2,11 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   server: {
-    port: 5173
-  }
+    proxy: {
+      "/ws": {
+        target: "ws://localhost:3000",
+        ws: true,
+      },
+    },
+  },
 });
